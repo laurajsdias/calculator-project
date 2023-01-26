4f5c38be02b45d5a4e7f4bde26d868d274fb01d9
Laura Dias <ldias@sangoma.com>
2023-01-26 17:37:04 -0300

    update main branch

--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-    region = "sa-east-1"
-}
-
-module "s3" {
-  source = "./s3"
-}
--- terraform/s3/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "s3_website_domain" {
-  value = aws_s3_bucket_website_configuration.calculadora.website_endpoint
-}
-
-
